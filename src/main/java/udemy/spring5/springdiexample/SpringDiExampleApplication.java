4346d80fba2fd521529460939bbd27c7da5481f7
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-04 12:10:03 +0530

    Initial Project Add

---
+++ src/main/java/udemy/spring5/springdiexample/SpringDiExampleApplication.java
@@ -0,0 +1,13 @@
+package udemy.spring5.springdiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiExampleApplication.class, args);
+	}
+
+}
