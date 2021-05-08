ceedcfb78a31aff74c6c9993b3cd16bccd6f293b
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-09 00:37:53 +0530

    Added XML based config.

--- src/main/java/udemy/spring5/springdiexample/config/GreetingServiceConfig.java
+++ src/main/java/udemy/spring5/springdiexample/config/GreetingServiceConfig.java
@@ -1,15 +1,13 @@
 package udemy.spring5.springdiexample.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import udemy.spring5.pets.PetService;
 import udemy.spring5.pets.PetServiceFactory;
 import udemy.spring5.springdiexample.repositories.EnglishGreetingRepository;
 import udemy.spring5.springdiexample.repositories.EnglishGreetingRepositoryImpl;
 import udemy.spring5.springdiexample.services.*;
 
+@ImportResource("classpath:spring-config.xml")
 @Configuration
 public class GreetingServiceConfig {
     @Bean
@@ -50,25 +48,23 @@ public class GreetingServiceConfig {
     I18nEnglishService I18nService(EnglishGreetingRepository englishGreetingRepository) {
         return new I18nEnglishService(englishGreetingRepository);
     }
+
     @Primary
     @Bean
     PrimaryGreetingService primaryGreetingService()
     {
         return new PrimaryGreetingService();
     }
+
     @Bean
     PropertyGreetingService propertyGreetingService()
     {
         return new PropertyGreetingService();
     }
+
     @Bean
     SetterGreetingService setterGreetingService()
     {
         return new SetterGreetingService();
-    }
-    @Bean
-    ConstructorGreetingService constructorGreetingService()
-    {
-        return new ConstructorGreetingService();
     }
 }
